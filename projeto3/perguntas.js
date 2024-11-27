995caa31bb1f105cac69bb52e0c65af418bca7dc
Warrenjuan <00001109170233sp@al.educacao.sp.gov.br>
2024-11-27 18:28:58 -0300

    Add files via upload

---
+++ projeto3/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Poderes',
+    'Qual é o poder do Batman?',
+    'Aquisitivo'
+)
+
+criaCartao(
+    'Identidade Secreta',
+    'Qual o Alter-Ego do homem Borracha?',
+    'Patrick O,brien.'
+)
+
+criaCartao(
+    'Animes',
+    'Quem foi o rei dos Piratas?',
+    'Gol D Roger.'
+)
+
+criaCartao(
+    'História dos Animes',
+    'Quem criou Dragon ball ?',
+    'Akira Toriyama'
+)
